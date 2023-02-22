5bee40ed9eae2ce84a87e4a40b5c18b305ca8f88
Kisielekw <dawkis1@outlook.com>
2023-02-22 11:57:44 +0000

    Added movement

--- Game1/Assets/Scripts/Player.cs
+++ Game1/Assets/Scripts/Player.cs
@@ -6,6 +6,7 @@ public class Player : MonoBehaviour
 {
     public float m_MaxHealth;
 
+    private const float speed = 0.15f;
 
     // Start is called before the first frame update
     void Start()
@@ -17,5 +18,39 @@ public class Player : MonoBehaviour
     void Update()
     {
         
+    }
+
+    private void FixedUpdate()
+    {
+        Movement();
+    }
+
+    /// <summary>
+    /// Method that contains all the movement for the player
+    /// </summary>
+    private void Movement()
+    {
+        
+        Vector3 direction = new Vector3();
+        if (Input.GetKey(KeyCode.A))
+        {
+            direction.x -= 1;
+        }
+        if (Input.GetKey(KeyCode.D))
+        {
+            direction.x += 1;
+        }
+        if (Input.GetKey(KeyCode.W))
+        {
+            direction.y += 1;
+        }
+        if (Input.GetKey(KeyCode.S))
+        {
+            direction.y -= 1;
+        }
+
+        direction.Normalize();
+
+        transform.position += new Vector3(direction.x * speed, direction.y * speed, direction.z);
     }
 }
