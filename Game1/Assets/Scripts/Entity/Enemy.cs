5b52c9001d2f4baaeeadd7f7bb1fe5ae11bb8cb5
Peter Garrod <p.jlgarrod@gmail.com>
2023-03-29 16:53:57 +0100

    Comment overhaul

--- Game1/Assets/Scripts/Entity/Enemy.cs
+++ Game1/Assets/Scripts/Entity/Enemy.cs
@@ -5,24 +5,40 @@ using UnityEngine;
 using UnityEngine.AI;
 
 /// <summary>
-/// Basic enemy capable of pathfinding to a target position
+/// Basic enemy capable of pathfinding to a target position.
 /// </summary>
 public class Enemy : Entity
 {
+    /// <summary>
+    /// Agent used to generate navigation path.
+    /// </summary>
+    /// <remarks>
+    /// Unity's built in pathfinding doesn't work too well with 2D scenes.
+    /// Path travelling should be handled by the entity not by the agent.
+    /// Utilizing built in transform updates results in unwanted changes to rotation and z position,
+    /// rendering the entity invisible to the player.
+    /// </remarks>
     private NavMeshAgent m_Agent;
 
     /// <summary>
-    /// Pathfinding goal
+    /// Pathfinding goal.
     /// </summary>
+    /// <remarks>
+    /// Currently incapable of automatically updating pathing target.
+    /// </remarks>
     public Transform target;
 
+    /// <summary>
+    /// Called on startup.
+    /// </summary>
+    /// <remarks>
+    /// Fetch and disable updates for <see cref="m_Agent"/>,
+    /// and set initial target position.
+    /// </remarks>
     void Start()
     {
         m_Agent = GetComponent<NavMeshAgent>();
 
-        // Unity's built in pathfinding doesn't work well with 2D
-        // Path travelling should be handled by the entity not by the navagent
-        // Utilizing built in transform updates results in unwanted changes to rotation and z position rendering the entity invisible to the player
         m_Agent.updateRotation = false;
         m_Agent.updateUpAxis = false;
         m_Agent.updatePosition = false;
@@ -30,6 +46,12 @@ public class Enemy : Entity
         m_Agent.SetDestination(target.position);        
     }
 
+    /// <summary>
+    /// Per frame update.
+    /// </summary>
+    /// <remarks>
+    /// Manually move to next position according to <see cref="m_Agent"/>.
+    /// </remarks>
     void Update()
     {
         // Manually traverse path
