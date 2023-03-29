d49ba4adc05f2e89e51ec48cf01cf6bfb0e919d6
Peter Garrod <p.jlgarrod@gmail.com>
2023-03-29 16:53:36 +0100

    Added comments

--- Game1/Assets/Scripts/Debug/DrawBox2D.cs
+++ Game1/Assets/Scripts/Debug/DrawBox2D.cs
@@ -2,37 +2,78 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
+/// <summary>
+/// Used to draw a <see cref="BoxCollider2D"/>.
+/// </summary>
+/// <remarks>
+/// Sourced from <a href="https://gamedev.stackexchange.com/questions/197313/show-colliders-in-a-build-game-in-unity">Bo Monroe</a> on Stack Overflow.
+/// Used for debugging collisions.
+/// </remarks>
 [RequireComponent(typeof(BoxCollider2D))]
-
 public class DrawBox2D : MonoBehaviour
 {
-    [SerializeField] private GameObject linePrefab;
-    LineRenderer lineRenderer;
-    BoxCollider2D boxCollider2D;
+    /// <summary>
+    /// Prefab line.
+    /// </summary>
+    /// <remarks>
+    /// Describes how the outline will look
+    /// </remarks>
+    [SerializeField]
+    private GameObject m_LinePrefab;
 
+    /// <summary>
+    /// Actual line renderer component used.
+    /// </summary>
+    [SerializeField]
+    private LineRenderer m_LineRenderer;
+
+    /// <summary>
+    /// Collider being drawn.
+    /// </summary>
+    [SerializeField]
+    private BoxCollider2D m_BoxCollider2D;
+
+    /// <summary>
+    /// Called on creation.
+    /// </summary>
+    /// <remarks>
+    /// Fetches <see cref="m_LineRenderer"/> and <see cref="m_BoxCollider2D"/> components.
+    /// </remarks>
     void Start()
     {
-        lineRenderer = Instantiate(linePrefab).GetComponent<LineRenderer>();
-        lineRenderer.transform.SetParent(transform);
-        lineRenderer.transform.localPosition = Vector3.zero;
-        boxCollider2D = GetComponent<BoxCollider2D>();
-       
+        m_LineRenderer = Instantiate(m_LinePrefab).GetComponent<LineRenderer>();
+        m_LineRenderer.transform.SetParent(transform);
+        m_LineRenderer.transform.localPosition = Vector3.zero;
+        m_BoxCollider2D = GetComponent<BoxCollider2D>();
     }
 
+    /// <summary>
+    /// Per frame update.
+    /// </summary>
+    /// <remarks>
+    /// Simply calls <see cref="HiliteBox"/> to update outline.
+    /// </remarks>
     void Update()
     {
         HiliteBox();
     }
 
+    /// <summary>
+    /// Draws box outline.
+    /// </summary>
+    /// <remarks>
+    /// Changed from <a href="https://gamedev.stackexchange.com/questions/197313/show-colliders-in-a-build-game-in-unity">source</a>
+    /// to account for collider offset.
+    /// </remarks>
     void HiliteBox()
     {
-        Vector3 offset = new Vector3(boxCollider2D.offset.x, boxCollider2D.offset.y, 0);
+        Vector3 offset = new Vector3(m_BoxCollider2D.offset.x, m_BoxCollider2D.offset.y, 0);
 
         Vector3[] positions = new Vector3[4];
-        positions[0] = transform.TransformPoint( new Vector3(boxCollider2D.size.x / 2.0f, boxCollider2D.size.y / 2.0f,0) + offset);
-        positions[1] = transform.TransformPoint(new Vector3(-boxCollider2D.size.x / 2.0f, boxCollider2D.size.y / 2.0f,0) + offset);
-        positions[2] = transform.TransformPoint(new Vector3(-boxCollider2D.size.x / 2.0f, -boxCollider2D.size.y / 2.0f,0) + offset);
-        positions[3] = transform.TransformPoint(new Vector3(boxCollider2D.size.x / 2.0f, -boxCollider2D.size.y / 2.0f,0) + offset);
-        lineRenderer.SetPositions(positions);
+        positions[0] = transform.TransformPoint( new Vector3(m_BoxCollider2D.size.x / 2.0f, m_BoxCollider2D.size.y / 2.0f,0) + offset);
+        positions[1] = transform.TransformPoint(new Vector3(-m_BoxCollider2D.size.x / 2.0f, m_BoxCollider2D.size.y / 2.0f,0) + offset);
+        positions[2] = transform.TransformPoint(new Vector3(-m_BoxCollider2D.size.x / 2.0f, -m_BoxCollider2D.size.y / 2.0f,0) + offset);
+        positions[3] = transform.TransformPoint(new Vector3(m_BoxCollider2D.size.x / 2.0f, -m_BoxCollider2D.size.y / 2.0f,0) + offset);
+        m_LineRenderer.SetPositions(positions);
     }
 }
