116682aaf191e2a51e3c11dd73413b073247b7a1
Peter Garrod <p.jlgarrod@gmail.com>
2023-03-29 16:26:38 +0100

    Comment overhaul

--- Game1/Assets/Scripts/Attacks/Attack.cs
+++ Game1/Assets/Scripts/Attacks/Attack.cs
@@ -6,73 +6,140 @@ using UnityEngine;
 using UnityEngine.Events;
 
 /// <summary>
-/// Generic attack
-/// Attacks are created as prefabs then are created from a <see cref="AttackInfo"/> template
-/// then constructed using the <see cref="Attack.Create(AttackInfo)"/> function.
+/// Generic attack.
 /// </summary>
+/// <remarks>
+/// Attacks are defined as prefabs, then created from an <see cref="AttackInfo"/> template,
+/// using the <see cref="Attack.Create(AttackInfo)"/> function.
+/// </remarks>
 [Serializable]
 public class Attack : MonoBehaviour
 {
     /// <summary>
     /// UID for the owner gameobject
     /// </summary>
+    /// <remarks>
+    /// Used to avoid attacks colliding with their owner.
+    /// </remarks>
+    /// <value>
+    /// Often a <see cref="GameObject"/> with an <see cref="Entity"/> component.
+    /// </value>
     public int OwnerID { get; private set; }
 
+    /// <summary>
+    /// Damage dealt.
+    /// </summary>
     [SerializeField]
     private float m_Damage;
 
+    /// <summary>
+    /// The travelspeed for this attack.
+    /// </summary>
+    /// <remarks>
+    /// Mostly applicable to projectiles.
+    /// </remarks>
     [SerializeField]
     private float m_Speed;
 
+    /// <summary>
+    /// How long in seconds attack lasts.
+    /// </summary>
+    /// <remarks>
+    /// Used with <see cref="Type.Timeout"/> and <see cref="m_End"/>.
+    /// </remarks>
     [SerializeField, Min(0f)]
     private float m_Duration;
 
     /// <summary>
-    /// Timestamp for timeout destruction
-    /// Set when entering scene
+    /// Timestamp for timeout destruction.
     /// </summary>
+    /// <value>
+    /// Determined by <see cref="m_Duration"/> and used for <see cref="Type.Timeout"/>.
+    /// </value>
     private float m_End;
 
-    [Serializable, Flags]
     /// <summary>
-    /// Conditions under which attack will destroy self
+    /// Conditions under which attack will destroy self.
     /// </summary>
+    [Serializable, Flags]
     public enum Type
     {
         /// <summary>
-        /// No destroy condition due to no flags set
+        /// No destroy conditions.
         /// </summary>
         Persistant = 0,
+
         /// <summary>
-        /// Destroys attack after set lifespan
+        /// Destroy after set time.
         /// </summary>
+        /// <remarks>
+        /// Makes use of <see cref="Attack.m_Duration"/> and <see cref="Attack.m_End"/>.
+        /// </remarks>
         Timeout = (1 << 0),
+
         /// <summary>
-        /// Destroy on hit
+        /// Destroy on successful hit with a <see cref="Entity"/>.
         /// </summary>
         OnHit = (1 << 2),
+
         /// <summary>
-        /// Destroy on any collision
+        /// Destroy on any collision, regardless of outcome.
         /// </summary>
         OnCollide = (1 << 3),
     }
 
+    /// <summary>
+    /// See <see cref="Type"/>.
+    /// </summary>
     [SerializeField]
     private Type m_Type;
 
     /// <summary>
-    /// Hashmap of each entity hit, keyed by UID
+    /// Hashmap of each entity hit.
     /// </summary>
+    /// <remarks>
+    /// Used for passing to <see cref="OnEnd"/> event,
+    /// and avoiding hitting entities multiple times.
+    /// </remarks>
+    /// <value>
+    /// <list type="table">
+    ///     <item>
+    ///         <term>Key</term>
+    ///         <description>
+    ///             UID of Entity.
+    ///         </description>
+    ///     </item>
+    ///     <item>
+    ///         <term>Value</term>
+    ///         <description>
+    ///             Reference to hit <see cref="Entity"/>
+    ///         </description>
+    ///     </item>
+    /// </list>
+    /// </value>
     private Dictionary<int, Entity> m_ObjectsHit;
 
     /// <summary>
-    /// Per hit event
+    /// Per hit event.
     /// </summary>
+    /// <remarks>
+    /// Called by <see cref="Attack.Hit(GameObject)"/> function.
+    /// </remarks>
+    /// <value>
+    /// Passes reference to hit entity.
+    /// </value>
+
     public UnityEvent<Entity> OnHit;
 
     /// <summary>
-    /// Destruction event, passes array of all entities hit
+    /// Destruction event.
     /// </summary>
+    /// <remarks>
+    /// Called by <see cref="OnDestroy"/> function.
+    /// </remarks>
+    /// <value>
+    /// Passes array of all entities hit over attack lifetime.
+    /// </value>
     public UnityEvent<Entity[]> OnEnd;
 
     public Attack()
@@ -80,11 +147,23 @@ public class Attack : MonoBehaviour
         m_ObjectsHit = new();
     }
 
+    /// <summary>
+    /// Called on creation.
+    /// </summary>
+    /// <remarks>
+    /// Sets the timestamp for <see cref="Type.Timeout"/>.
+    /// </remarks>
     void Start()
     {
         m_End = Time.time + m_Duration;
     }
 
+    /// <summary>
+    /// Per frame update.
+    /// </summary>
+    /// <remarks>
+    /// Checks for <see cref="Type.Timeout"/> condition.
+    /// </remarks>
     void Update()
     {
         // Check lifespan
@@ -92,6 +171,12 @@ public class Attack : MonoBehaviour
             Destroy(gameObject);
     }
 
+    /// <summary>
+    /// Physics synced update.
+    /// </summary>
+    /// <remarks>
+    /// Moves attack by <see cref="m_Speed"/> along local X axis.
+    /// </remarks>
     void FixedUpdate()
     {
         // Move if object has speed
@@ -100,11 +185,14 @@ public class Attack : MonoBehaviour
     }
 
     /// <summary>
-    /// Called for each intersecting collider during physics update.
-    /// Could use OnTrigger enter to avoid multi collisions, but would lead to issue
-    /// where an enitity who lost the untargetable state whilst in an attack collider
-    /// would ignore further collisions and not take damage
+    /// Called for each intersecting collider during physics update,
+    /// passing each into the <see cref="Hit"/> function. 
     /// </summary>
+    /// <remarks>
+    /// Could use OnTrigger enter to avoid multi collisions, but would lead to issue
+    /// where an enitity who lost the untargetable state whilst in an attack collider,
+    /// ignoring further collisions and not taking damage.
+    /// </remarks>
     private void OnTriggerStay2D(Collider2D collider)
     {
         // Funky conditional to avoid repeated calls to Destroy self from overlap of OnHit and OnCollide conditions
@@ -116,10 +204,33 @@ public class Attack : MonoBehaviour
     }
 
     /// <summary>
-    /// Called per collision
+    /// Called per collision.
     /// </summary>
+    /// <remarks>
+    /// checks if target object is an entity,
+    /// and if a damaging collision took place calling
+    /// <see cref="OnHit"/> event if necessary.
+    /// </remarks>
     /// <param name="collisionObject">collider</param>
-    /// <returns>True if collided with targetable entity</returns>
+    /// <returns>
+    /// <list type="table">
+    ///     <listheader>
+    ///         <term>State</term>
+    ///         <description>Effect</description>
+    ///     </listheader>
+    ///     <item>
+    ///         <term>True</term>
+    ///         <description>
+    ///             Attack collided with targetable entity.
+    ///         </description>
+    ///     </item>
+    ///     <item>
+    ///         <term>False</term>
+    ///          <description>
+    ///            Collider was  either not an entity or un-targetable.
+    ///          </description>
+    ///     </item>
+    /// </list>
     private bool Hit(GameObject collisionObject)
     {
         // Check object ID to avoid hitting owner or multiple collisions with same object
@@ -147,6 +258,12 @@ public class Attack : MonoBehaviour
         return true;
     }
 
+    /// <summary>
+    /// Called on attack destruction.
+    /// </summary>
+    /// <remarks>
+    /// <see cref="OnEnd"/> called here.
+    /// </remarks>
     void OnDestroy()
     {
         // Before destruction post event with entities hit
@@ -154,8 +271,13 @@ public class Attack : MonoBehaviour
     }
 
     /// <summary>
-    /// Creates and returns new attack from given template
+    /// Psuedo factory, creating attacks from templates.
     /// </summary>
+    /// <remarks>
+    /// Had difficulty using a true factory due to how prefabs and objects are constructed by Unity.
+    /// </remarks>
+    /// <param name="info"><see cref="AttackInfo"/> template.</param>
+    /// <returns>Created attack object.</returns>
     public static Attack Create(AttackInfo info)
     {
         Transform parent = info.Owner.transform;
@@ -175,37 +297,60 @@ public class Attack : MonoBehaviour
 }
 
 /// <summary>
-/// Struct container, describing values to be set
-/// Used to export additional variables on an imported prefab and created using the <see cref="Attack.Create(AttackInfo)"/> function.
+/// Struct container, describing an <see cref="Attack"/>.
 /// </summary>
+/// <remarks>
+/// Used to export additional variables on an imported prefab.
+/// Stored <see cref="Attack"/> created using <see cref="Attack.Create(AttackInfo)"/> function.
+/// </remarks>
 [Serializable]
 public struct AttackInfo
 {
     /// <summary>
-    /// Owner for the attack being spawned
+    /// See <see cref="Attack.Owner"/>.
     /// </summary>
     [HideInInspector]
     public GameObject Owner;
 
     /// <summary>
-    /// Prefab describing actual attack to be spawned
+    /// Prefab describing actual attack to be spawned.
     /// </summary>
+    /// <remarks>
+    /// Prefab can be used to define hitboxes, logic, etc.
+    /// </remarks>
     public Attack Prefab;
 
     /// <summary>
-    /// On hit event listeners
+    /// See <see cref="Attack.OnHit"/>.
     /// </summary>
     public UnityEvent<Entity> OnHit;
 
     /// <summary>
-    /// Attack end event listeners
+    /// See <see cref="Attack.OnEnd"/>.
     /// </summary>
     public UnityEvent<Entity[]> OnEnd;    
 
     /// <summary>
-    /// Determines the parent for the spawned attack object
-    /// When false the owner will be set as the parent object
-    /// When true attack will become a child of the owners parent
+    /// Determines the parent for the spawned attack object.
     /// </summary>
+    /// <remarks>
+    /// Often used for projectiles, which travel independant of the <see cref="AttackInfo.Owner"/>.
+    /// </remarks>
+    /// <value>
+    /// <list type="table">
+    ///     <item>
+    ///         <term>True</term>
+    ///         <description>
+    ///             Attack will become a child of the owners parent.
+    ///         </description>
+    ///     </item>
+    ///     <item>
+    ///         <term>False</term>
+    ///          <description>
+    ///             Owner will be set as the parent object.
+    ///          </description>
+    ///     </item>
+    /// </list>
+    /// </value>
     public bool UseWorldSpace;
 }
